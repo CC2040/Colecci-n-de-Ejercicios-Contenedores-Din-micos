06d8a059c1e0fe70f2bab86876f7333f1abaf734
CC2040 <ccondesp@myuax.com>
2024-12-18 18:06:04 +0100

    Ejercicio3

--- Ejercicio3/algoritmos.cpp
+++ Ejercicio3/algoritmos.cpp
@@ -1,5 +1,18 @@
-//
-// Created by carlo on 16/12/2024.
-//
-
 #include "algoritmos.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+void algoritmos() {
+    std::vector<int> numeros = {50, 20, 60, 10, 30, 40};
+    std::sort(numeros.begin(), numeros.end());
+
+    std::cout << "\nEjercicio 3"<< std::endl;
+    for(int num : numeros) {
+        std::cout << num <<" ";
+    }
+
+    int maxNum = *std::max_element(numeros.begin(), numeros.end());
+    std::cout << "\nNumero maximo: " << maxNum << std::endl;
+
+}
--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include "Ejercicio1/vector.h"
 #include "Ejercicio2/secuencias.h"
+#include "Ejercicio3/algoritmos.h"
 
 int main() {
     /*Ejercicio 1*/
     vector();
     /*Ejercicio2*/
     secuencias();
+    /*Ejercicio3*/
+    algoritmos();
 
     return 0;
 }
